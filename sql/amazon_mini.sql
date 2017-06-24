00f2f967e4def04dbdbc52a4231377185cf2bc55
piaaaa-nyk <nayakprya123@gmail.com>
2017-06-24 13:23:07 +0530

    Database created

---
+++ sql/amazon_mini.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jun 24, 2017 at 09:47 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amazon_mini`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Author`
+--
+
+CREATE TABLE `Author` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Books`
+--
+
+CREATE TABLE `Books` (
+  `book_id` int(11) NOT NULL,
+  `sub_id` int(11) NOT NULL,
+  `book_name` varchar(150) NOT NULL,
+  `description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `price_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `lang_id` int(11) NOT NULL,
+  `pages` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `fulfilled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Carts`
+--
+
+CREATE TABLE `Carts` (
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `ordered` tinyint(1) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Language`
+--
+
+CREATE TABLE `Language` (
+  `lang_id` int(11) NOT NULL,
+  `language` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Author`
+--
+ALTER TABLE `Author`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `Books`
+--
+ALTER TABLE `Books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `Language`
+--
+ALTER TABLE `Language`
+  ADD PRIMARY KEY (`lang_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Author`
+--
+ALTER TABLE `Author`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Books`
+--
+ALTER TABLE `Books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Language`
+--
+ALTER TABLE `Language`
+  MODIFY `lang_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
