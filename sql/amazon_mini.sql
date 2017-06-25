8d75c7c80c8850caa05bb07a93a5346be1d5feb7
sunaina25 <punyanisunaina38@gmail.com>
2017-06-25 13:07:52 +0530

    sql added

--- sql/amazon_mini.sql
+++ sql/amazon_mini.sql
@@ -1,12 +1,11 @@
---okay okay
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Jun 24, 2017 at 09:47 AM
--- Server version: 5.6.35
--- PHP Version: 7.0.15
+-- Host: 127.0.0.1
+-- Generation Time: Jun 25, 2017 at 09:35 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -24,10 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Author`
+-- Table structure for table `author`
 --
 
-CREATE TABLE `Author` (
+CREATE TABLE `author` (
   `author_id` int(11) NOT NULL,
   `author_name` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -35,10 +34,10 @@ CREATE TABLE `Author` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Books`
+-- Table structure for table `books`
 --
 
-CREATE TABLE `Books` (
+CREATE TABLE `books` (
   `book_id` int(11) NOT NULL,
   `sub_id` int(11) NOT NULL,
   `book_name` varchar(150) NOT NULL,
@@ -49,16 +48,18 @@ CREATE TABLE `Books` (
   `lang_id` int(11) NOT NULL,
   `pages` int(11) NOT NULL,
   `company_id` int(11) NOT NULL,
-  `fulfilled` tinyint(1) NOT NULL
+  `fulfilled` tinyint(1) NOT NULL,
+  `sold` int(11) NOT NULL,
+  `available` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Carts`
+-- Table structure for table `carts`
 --
 
-CREATE TABLE `Carts` (
+CREATE TABLE `carts` (
   `user_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   `ordered` tinyint(1) NOT NULL,
@@ -68,63 +69,189 @@ CREATE TABLE `Carts` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Language`
+-- Table structure for table `category`
 --
 
-CREATE TABLE `Language` (
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_name` varchar(150) NOT NULL,
+  `company_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language`
+--
+
+CREATE TABLE `language` (
   `lang_id` int(11) NOT NULL,
   `language` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `price_range`
+--
+
+CREATE TABLE `price_range` (
+  `price_id` int(11) NOT NULL,
+  `starting_price` bigint(20) NOT NULL,
+  `ending_price` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_category`
+--
+
+CREATE TABLE `sub_category` (
+  `category_id` int(11) DEFAULT NULL,
+  `sub_id` int(11) DEFAULT NULL,
+  `sub_name` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `mobile_num` varchar(10) NOT NULL,
+  `email_id` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `cart_items` int(11) NOT NULL,
+  `grand_total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `Author`
+-- Indexes for table `author`
 --
-ALTER TABLE `Author`
-  ADD PRIMARY KEY (`author_id`);
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`author_id`),
+  ADD UNIQUE KEY `author_id` (`author_id`);
 
 --
--- Indexes for table `Books`
+-- Indexes for table `books`
 --
-ALTER TABLE `Books`
-  ADD PRIMARY KEY (`book_id`);
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `books_ibfk_1` (`lang_id`),
+  ADD KEY `books_ibfk_2` (`author_id`);
 
 --
--- Indexes for table `Language`
+-- Indexes for table `category`
 --
-ALTER TABLE `Language`
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`),
+  ADD UNIQUE KEY `category_name` (`category_name`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `language`
+--
+ALTER TABLE `language`
   ADD PRIMARY KEY (`lang_id`);
+
+--
+-- Indexes for table `price_range`
+--
+ALTER TABLE `price_range`
+  ADD PRIMARY KEY (`price_id`);
+
+--
+-- Indexes for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email_id` (`email_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Author`
+-- AUTO_INCREMENT for table `author`
 --
-ALTER TABLE `Author`
+ALTER TABLE `author`
   MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `Books`
+-- AUTO_INCREMENT for table `books`
 --
-ALTER TABLE `Books`
+ALTER TABLE `books`
   MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `Language`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `Language`
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `language`
+--
+ALTER TABLE `language`
   MODIFY `lang_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `price_range`
+--
+ALTER TABLE `price_range`
+  MODIFY `price_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`lang_id`) REFERENCES `language` (`lang_id`),
+  ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `author` (`author_id`);
+
+--
+-- Constraints for table `sub_category`
+--
+ALTER TABLE `sub_category`
+  ADD CONSTRAINT `sub_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
---
--- Constraints for table `Books`
---
-ALTER TABLE `Books`
-  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`lang_id`) REFERENCES `Language` (`lang_id`),
-  ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `Author` (`author_id`);
-
